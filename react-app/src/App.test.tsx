fe2a24f880071f8a70be26b27d93ef4710f55ab1
heyho00 <heyho9292@gmail.com>
2023-02-27 15:41:42 +0900

    hook mock

--- react-app/src/App.test.tsx
+++ react-app/src/App.test.tsx
@@ -1,8 +1,21 @@
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// describe('App ', () => {
+//   it('renders without crash', () => {
+//     render(<App />);
+//   });
+// });
+
+import { render, screen } from '@testing-library/react';
+import fixtures from '../fixtures';
 import App from './App';
 
-describe('App ', () => {
-  it('renders without crash', () => {
-    render(<App />);
-  });
+// jest.mock('./hooks/useFetchRestaurants.ts', () => () => fixtures.products);
+jest.mock('./hooks/useFetchRestaurants.ts');
+
+test('App', () => {
+  render(<App />);
+
+  screen.getByText('메가반점');
 });
