11769477183a0e9d933ed61b5dcfb72d303accdf
heyho00 <heyho9292@gmail.com>
2023-02-27 14:57:59 +0900

    mock clear

--- react-app/tests/TextField.test.tsx
+++ react-app/tests/TextField.test.tsx
@@ -1,35 +1,62 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import TextField from '../src/components/TextField';
 
-test('TextField', () => {
+const context = describe;
+
+describe('TextField', () => {
   // given
   const label = 'Name';
   const text = 'Tester';
 
   const setText = jest.fn();
 
-  // when
-  render(
-    <TextField
-      label={label}
-      placeholder="name is harry"
-      text={text}
-      setText={setText}
-    />,
-  );
-
-  // then 화면이 나와야 함
-  screen.getByLabelText('Name');
-  screen.getAllByDisplayValue(text);
-  screen.getAllByPlaceholderText(/name/);
-
-  // --------
-  // when
-  const input = screen.getByLabelText('Name');
-  fireEvent.change(input, {
-    target: { value: 'New Name' },
+  beforeEach(() => {
+    // setText.mockClear();
+    jest.clearAllMocks();
   });
 
-  expect(setText).toBeCalled();
-  expect(setText).toBeCalledWith('New Name');
+  function renderTextField() {
+    render(
+      <TextField
+        label={label}
+        placeholder="name is harry"
+        text={text}
+        setText={setText}
+      />,
+    );
+  }
+
+  function inputText(value:string) {
+    fireEvent.change(screen.getByLabelText('Name'), {
+      target: { value },
+    });
+  }
+
+  it('renders elements', () => {
+    // when
+    renderTextField();
+
+    // then 화면이 나와야 함
+    screen.getByLabelText(label);
+    screen.getAllByDisplayValue(text);
+    screen.getAllByPlaceholderText(/name/);
+  });
+
+  // --------
+
+  // context: 입력했을때
+  context('when user enters name', () => {
+    beforeEach(() => {
+      // given
+      renderTextField();
+    });
+
+    it('calls "setText" handler', () => {
+      // when
+      inputText('New Name');
+
+      // then
+      expect(setText).toBeCalledWith('New Name');
+    });
+  });
 });
